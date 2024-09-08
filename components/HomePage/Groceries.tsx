e134f4d9db5fddfd6f2ae63a502319d8a2f3f32e
Nworlu <nworluegbe@gmail.com>
2024-09-09 00:14:38 +0100

    fix: little changes

--- components/HomePage/Groceries.tsx
+++ components/HomePage/Groceries.tsx
@@ -20,13 +20,13 @@ const Groceries = () => {
       </div>
       <div className="w-full flex lg:flex-row flex-col items-center lg:items-start justify-between xl:justify-center gap-6 xl:gap-20">
         <Image
-          src="/Picture1.png"
+          src="/Picture5.png"
           alt="grocery"
           width={400}
           height={400}
           className="lg:w-[540px] md:w-[480px] w-[400px] rounded-lg"
         />
-        <div className="grid grid-cols-1 gap-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           <div
             data-aos="fade-right"
             data-aos-duration="500"
@@ -38,8 +38,7 @@ const Groceries = () => {
                 {/* Variety of products */}
               </h2>
               <p className="text-[15px] text-[#4F4F4F] font-medium">
-                {/* Get access to a wide range of products from your favourite
-                brands in one convenient place. */}
+                Research and Data Analysis
               </p>
             </div>
             <img src="/icon_g.svg" alt="" className="ml-auto w-[70px]" />
@@ -55,9 +54,55 @@ const Groceries = () => {
                 {/* Affordable Prices */}
               </h2>
               <p className="text-[15px] text-[#4F4F4F] font-medium">
-                {/* Our platform supports local farmers, distributors and
-                greenhouses there by giving you direct wholesale prices and
-                swift delivery at your door step. */}
+                Training & Workshop Facilitaion
+              </p>
+            </div>
+            <img src="/icon_g.svg" alt="" className="ml-auto w-[70px]" />
+          </div>
+          <div
+            data-aos="fade-right"
+            data-aos-duration="500"
+            data-aos-delay="300"
+            className="bg-[#1F7B1033] rounded-2xl flex flex-col items-center md:py-10 py-6 px-4 md:px-12 gap-6 max-w-[25rem] md:max-w-[34rem] "
+          >
+            <div className="flex flex-col gap-3">
+              <h2 className="text-[#221919] text-[18px] font-semibold">
+                {/* Affordable Prices */}
+              </h2>
+              <p className="text-[15px] text-[#4F4F4F] font-medium">
+                Project Implementation and Management
+              </p>
+            </div>
+            <img src="/icon_g.svg" alt="" className="ml-auto w-[70px]" />
+          </div>
+          <div
+            data-aos="fade-right"
+            data-aos-duration="500"
+            data-aos-delay="300"
+            className="bg-[#1F7B1033] rounded-2xl flex flex-col items-center md:py-10 py-6 px-4 md:px-12 gap-6 max-w-[25rem] md:max-w-[34rem] "
+          >
+            <div className="flex flex-col gap-3">
+              <h2 className="text-[#221919] text-[18px] font-semibold">
+                {/* Affordable Prices */}
+              </h2>
+              <p className="text-[15px] text-[#4F4F4F] font-medium">
+                Monitoring and Evaluation
+              </p>
+            </div>
+            <img src="/icon_g.svg" alt="" className="ml-auto w-[70px]" />
+          </div>
+          <div
+            data-aos="fade-right"
+            data-aos-duration="500"
+            data-aos-delay="300"
+            className="bg-[#1F7B1033] rounded-2xl flex flex-col items-center md:py-10 py-6 px-4 md:px-12 gap-6 max-w-[25rem] md:max-w-[34rem] "
+          >
+            <div className="flex flex-col gap-3">
+              <h2 className="text-[#221919] text-[18px] font-semibold">
+                {/* Affordable Prices */}
+              </h2>
+              <p className="text-[15px] text-[#4F4F4F] font-medium">
+                Community Mobilization/Development
               </p>
             </div>
             <img src="/icon_g.svg" alt="" className="ml-auto w-[70px]" />
