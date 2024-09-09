3e904dc2108dd38824743b5d3077f85d03157272
okpe onoja <okpeonoja18@gmail.com>
2024-09-09 01:35:00 +0100

    feat: image issue

--- components/ArtisansTraining/SkillSet.tsx
+++ components/ArtisansTraining/SkillSet.tsx
@@ -20,7 +20,7 @@ const options = {
   drag: "free",
   focus: "center",
   arrows: false,
-  width: "98%",
+  width: "100%",
   perPage: 4,
   autoScroll: {
     speed: 2,
@@ -44,44 +44,27 @@ const options = {
 const SkillSet = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center pt-16 md:pt-20 gap-16">
-      {/* <div className="flex flex-col gap-3 items-start w-full md:w-[90%] lg:w-[70%] px-5 md:pl-20">
-        <h5
-          className={`bg-[#F5FBF6] rounded-md font-semibold text-[13px] text-[#FF6525] py-1 mx-auto sm:mx-0 sm:ml-24 px-4 ${inter.className}`}
-        >
-          Why Cdrt?
-        </h5>
-        <h1
-          className={` text-[#14183E] font-bold text-[19px] mx-auto text-center sm:text-left sm:mx-0 sm:text-[28px] ${sora.className}`}
-        >
-          Reimagining the African Dream
-        </h1>
-        <p
-          className={`text-[#5E6282] leading-[1.7] font-normal text-[15px] sm:text-[16px] ${poppins.className}`}
-        >
-          The skills gap in agriculture and vocational trades is a real barrier
-          to both individual success and economic growth. That&apos;s why we&apos;re here:
-          to bridge that gap and empower you to achieve a fulfilling career in a
-          field you&apos;re passionate about. Here&apos;s how we make a difference:
-        </p>
-      </div> */}
-
       <Splide options={options} extensions={{ AutoScroll }}>
         {skillImages.map((item, index) => (
-           <SplideSlide
-           key={index}
-           style={{
-             overflow: "hidden",
-             margin: "2rem",
-           }}
-         >
-          <Image
+          <SplideSlide
             key={index}
-            src={item}
-            alt="Skill"
-            width={500}
-            height={500}
-            className="lg:w-full h-[500px] rounded-[10px]"
-          />
+            style={{
+              overflow: "hidden",
+              margin: "1rem",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <div className="relative w-[300px] h-[300px]">
+              <Image
+                src={item}
+                alt="Skill"
+                layout="responsive" 
+                width={300}
+                height={300}
+                className="object-cover rounded-[10px]"
+              />
+            </div>
           </SplideSlide>
         ))}
       </Splide>
